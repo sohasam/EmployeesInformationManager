e66ab188e4088b3d4e560a857f194d7f66f14f03
sohasam <58265170+sohasam@users.noreply.github.com>
2020-06-17 15:46:26 +0200

    recyclerview

---
+++ app/src/main/java/com/example/employeesinformationmanager/adapter/InformationListAdapter.java
@@ -0,0 +1,98 @@
+package com.example.employeesinformationmanager.adapter;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.employeesinformationmanager.R;
+import com.example.employeesinformationmanager.models.EmployeeModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InformationListAdapter extends RecyclerView.Adapter<InformationListAdapter.InformationListViewHolder> {
+    private List<EmployeeModel> employeesArray=new ArrayList<>();;
+    private Context context;
+
+    public InformationListAdapter(List<EmployeeModel> employeesArray, Context context) {
+        this.employeesArray = employeesArray;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public InformationListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        InformationListViewHolder informationListViewHolder;
+        LayoutInflater inflater =(LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        informationListViewHolder = new InformationListAdapter.InformationListViewHolder( inflater.inflate(R.layout.list_item , parent,false));
+        return informationListViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull InformationListViewHolder holder, int position) {
+
+
+        holder.nameTextView.setText(employeesArray.get(position).getName());
+        holder.emailTextView.setText(employeesArray.get(position).getEmail());
+
+        //img
+        holder.editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("TAG123", "onClick:editButton ");
+            }
+        });
+        holder.deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("TAG123", "onClick:deleteButton ");
+            }
+        });
+
+        holder.constraintLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("TAG123", "onClick: ");
+            }
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return employeesArray.size();
+    }
+
+    public class InformationListViewHolder extends RecyclerView.ViewHolder {
+
+      private   TextView nameTextView;
+      private   TextView emailTextView;
+      private   Button  editButton;
+      private   Button  deleteButton;
+
+       // ImageView img;
+        View layoutView;
+        ConstraintLayout constraintLayout;
+        public InformationListViewHolder(@NonNull View itemView) {
+            super(itemView);
+            /// find by id
+            layoutView = itemView;
+            constraintLayout =itemView.findViewById(R.id.constrain_list_item);
+            nameTextView  = itemView.findViewById(R.id.text_view_name);
+            emailTextView  = itemView.findViewById(R.id.text_view_email);
+            editButton  = itemView.findViewById(R.id.btn_edit);
+            deleteButton  = itemView.findViewById(R.id.btn_delete);
+              //  img =  itemView.findViewById(R.id.imageView) ;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/employeesinformationmanager/views/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.employeesinformationmanager.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.employeesinformationmanager.R;
+import com.example.employeesinformationmanager.adapter.InformationListAdapter;
+import com.example.employeesinformationmanager.models.EmployeeModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<EmployeeModel> personsArray= new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView= findViewById(R.id.recycler_View_employees_information);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        EmployeeModel emp1 = new EmployeeModel("soha","soha@gmail.coooooooooooooom");
+        EmployeeModel emp2 = new EmployeeModel("samar","soha@gmail.coooooooooooooom");
+
+        personsArray.add(emp1);
+        personsArray.add(emp2);
+        personsArray.add(emp1);
+        personsArray.add(emp2);
+        personsArray.add(emp1);
+        personsArray.add(emp2);
+        personsArray.add(emp1);
+        personsArray.add(emp2);
+        personsArray.add(emp1);
+        personsArray.add(emp1);
+        recyclerView.setAdapter(new InformationListAdapter(personsArray,this) );
+    }
+}
